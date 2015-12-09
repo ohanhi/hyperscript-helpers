ffeae64f0a2429de811255c38675b5c75cfe46ff
ohanhi <ossi.hanhinen@gmail.com>
2015-12-09 10:03:02 +0200

    Add main tag

--- src/index.js
+++ src/index.js
@@ -28,11 +28,11 @@ const TAG_NAMES = [
   'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form',
   'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html',
   'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'keygen', 'label', 'legend',
-  'li', 'link', 'map', 'mark', 'menu', 'meta', 'nav', 'noscript', 'object',
-  'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'q', 'rp', 'rt', 'ruby', 's',
-  'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strong',
-  'style', 'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th',
-  'thead', 'title', 'tr', 'u', 'ul', 'video'
+  'li', 'link', 'main', 'map', 'mark', 'menu', 'meta', 'nav', 'noscript',
+  'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'q', 'rp', 'rt',
+  'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span',
+  'strong', 'style', 'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot',
+  'th', 'thead', 'title', 'tr', 'u', 'ul', 'video'
 ];
 
 export default
